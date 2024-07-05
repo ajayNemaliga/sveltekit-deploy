964194e04bf8d4feaeb736861d424cc93086344a
Ajay <nemaligaajayreddy80@gmail.com>
2024-07-05 08:52:48 +0530

    add .gitattributes file

---
+++ src/lib/table.svelte
@@ -0,0 +1,1 @@
+<h1>hello table</h1>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Navvbar from "$lib/navvbar.svelte";
+    import Footer from "$lib/footer.svelte";
+    import "../routes/app.css";
+</script>
+
+<Navvbar/>
+<slot/>
+<Footer/>
